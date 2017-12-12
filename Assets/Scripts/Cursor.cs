358a674931ac3d57de4115505e618db5af91235c
ASPURRELL <acspuzz@gmail.com>
2017-12-12 17:23:04 +0000

    Lecture 12 - Click to move

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class Cursor : MonoBehaviour {
 
-    Camera m_MainCamera;
     // Use this for initialization
     void Start () {
-        m_MainCamera = Camera.main;
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetMouseButtonDown(0))
-        {
-            print(GetComponent<CameraRaycaster>().layerHit);
-        }
 	}
 }
