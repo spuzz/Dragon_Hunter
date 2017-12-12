74f28274048ada51f27eeea870ffa2f547cddec4
ASPURRELL <acspuzz@gmail.com>
2017-12-12 14:57:28 +0000

    lecture 10 - Raycasting

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+    Camera m_MainCamera;
+    // Use this for initialization
+    void Start () {
+        m_MainCamera = Camera.main;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0))
+        {
+            print(GetComponent<CameraRaycaster>().layerHit);
+        }
+	}
+}
